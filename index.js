099b2ed10dd02153894e494f80ca6abd8294485e
Shubham7350 <90132010+Shubham7350@users.noreply.github.com>
2022-11-05 23:18:01 +0530

    songs data added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert("working");
+let data = [
+  {
+    songName: "Downfall",
+    artist: "NCS",
+    image: "publicimagesdownfall.jpg",
+    path: "publicsongsdownfall-21371.mp3",
+  },
+  {
+    songName: "Happy Day",
+    artist: "NCS",
+    image: "publicimageshappdyDay.jpg",
+    path: "publicsongshappy-day-113985.mp3",
+  },
+  {
+    songName: "Ted",
+    artist: "NCS",
+    image: "publicimages\ted.jpg",
+    path: "publicsongs\ted-123989.mp3",
+  },
+];
